ae6071b67e8dcaefa4d4c4273256670c3a3ea291
caando <zhangjikun907475@gmail.com>
2025-01-24 02:47:11 +0800

    feat: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -28,28 +30,54 @@ public class Duke {
         output(" Bye. Hope to see you again soon!");
         output(LINE_SEPARATOR);
     }
-
-    private static void echo(String message) {
-        output(" " + message);
+    /**
+     * Adds a new task to the provided list of records and displays a confirmation message.
+     * The message is printed in the format: "added: message", followed by a line separator.
+     *
+     * @param records A list of strings representing tasks. The new task (message) will be added to this list.
+     * @param message The task to be added to the list of records.
+     */
+    private static void add(List<String> records, String message) {
+        records.add(message);
+        output(String.format(" added: %s", message));
         output(LINE_SEPARATOR);
     }
 
-    private static void receiveCommand() {
+    /**
+     * Displays a numbered list of tasks from the provided list of records.
+     * The list is formatted with each task preceded by its corresponding number.
+     *
+     * @param records A list of strings representing tasks to be displayed.
+     *                Each string in the list will be printed with a number in the format "1. task",
+     *                "2. task", etc.
+     */
+    public static void list(List<String> records) {
+        for (int i = 0; i < records.size(); i++) {
+            output(String.format(" %d. %s", i + 1, records.get(i)));
+        }
+        output(LINE_SEPARATOR);
+    }
+
+    private static void receiveCommand(List<String> records) {
         String command;
         try (Scanner scanner = new Scanner(System.in)) {
             command = scanner.nextLine();
             output(LINE_SEPARATOR);
-            executeCommand(command);
+            executeCommand(records, command);
         }
     }
 
-    private static void executeCommand(String command) {
+    private static void executeCommand(List<String> records, String command) {
         switch (command) {
         case "bye" -> {
         }
+        case "list" -> {
+            list(records);
+            receiveCommand(records);
+        }
         default -> {
-            echo(command);
-            receiveCommand();
+            add(records, command);
+            receiveCommand(records);
         }
         }
     }
@@ -57,7 +85,8 @@ public class Duke {
     public static void main(String[] args) {
         start();
         greet();
-        receiveCommand();
+        ArrayList<String> records = new ArrayList<>();
+        receiveCommand(records);
         bye();
     }
 }
