dac30762974f8c423c632be383ebee633bd38979
caando <zhangjikun907475@gmail.com>
2025-01-24 03:57:36 +0800

    feat: Add AddCommand

---
+++ src/main/java/command/AddCommand.java
@@ -0,0 +1,23 @@
+package command;
+
+import task.TaskList;
+
+public class AddCommand implements Command {
+
+    private final String taskDescription;
+
+    private AddCommand(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public static AddCommand parse(String command) {
+        return new AddCommand(command.trim());
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+        taskList.add(taskDescription);
+        System.out.println("     added: " + taskDescription);
+        System.out.println("    ____________________________________________________________");
+    }
+}
