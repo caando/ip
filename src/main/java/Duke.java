f8d0ea98bddf24aa953eab859466a9e8c94d7ae7
caando <zhangjikun907475@gmail.com>
2025-01-24 02:06:04 +0800

    feat: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,12 +4,27 @@
  * project.
  */
 public class Duke {
+
+    private static final String LINE_SEPARATOR = "____________________________________________________________";
+
+    private static void start() {
+        System.out.println(LINE_SEPARATOR);
+    }
+
+    private static void greet() {
+        System.out.println(" Hello! I'm Mr Meeseeks");
+        System.out.println(" What can I do for you?");
+        System.out.println(LINE_SEPARATOR);
+    }
+
+    private static void bye() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(LINE_SEPARATOR);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        start();
+        greet();
+        bye();
     }
 }
