818e8a3bbc4bf122bdad7c6a29466d4fff57994d
caando <zhangjikun907475@gmail.com>
2025-01-24 03:56:32 +0800

    feat: Add task

---
+++ src/main/java/task/Task.java
@@ -0,0 +1,29 @@
+package task;
+
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // Mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+}
