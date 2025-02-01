b898ca2ecf728c31bc4a4a0bc10d71f41f515281
caando <zhangjikun907475@gmail.com>
2025-02-01 22:57:51 +0800

    doc: Add javadoc for Duke, Utils and Parser

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -11,8 +11,25 @@ import duke.task.TaskList;
 import duke.ui.Cli;
 import duke.ui.Ui;
 
+/**
+ * The main entry point for the Duke application.
+ * <p>
+ * This class initializes and runs the Duke task manager application. It sets up the storage, task list,
+ * and user interface, then enters an interactive loop where user commands are parsed, executed, and handled.
+ * The application continues running until the user inputs the "bye" command, at which point the program terminates.
+ */
 public class Duke {
 
+    /**
+     * The main method that runs the Duke application.
+     * <p>
+     * It sets up the necessary components such as the storage, task list, and user interface. It attempts
+     * to load existing tasks from storage, and then enters a loop where user input is continually parsed and
+     * processed. Commands are executed accordingly, and errors are displayed if they occur. The program terminates
+     * when the user inputs the "bye" command.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         Storage storage = new FileStorage("./data/duke.txt");
         TaskList tasks = new TaskList();
