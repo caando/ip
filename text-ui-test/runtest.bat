0c5ee35195e1406f1b06b34dc406a1812eafb8c1
caando <zhangjikun907475@gmail.com>
2025-02-10 01:02:00 +0800

    fix: Fix compile command in runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -15,7 +15,7 @@ REM copy initial data to state
 xcopy /e /i "data-initial" "data"
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\**\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
